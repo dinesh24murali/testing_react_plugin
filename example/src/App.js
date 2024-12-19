025c15cbd294652a5621baafa17ff1c18465a396
dinesh24murali <dinesh24murali@gmail.com>
2024-12-19 06:43:35 +0530

    Add: updated packages

--- example/src/App.js
+++ example/src/App.js
@@ -2,8 +2,8 @@ import { SampleComponent } from 'testing_react_plugin';
 
 function App() {
   return (
-    <div className="App">
-      <SampleComponent text='German' />
+    <div>
+        <SampleComponent text="sample" />
     </div>
   );
 }
